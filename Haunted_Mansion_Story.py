7fe666c9e9bc35ef772efd9848a1129c019780aa
niilokekkonen <niilo.kekkonen@metropolia.fi>
2025-10-01 09:38:47 +0300

    Initial commit

---
+++ Haunted_Mansion_Story.py
@@ -0,0 +1,21 @@
+def show_story():
+    print(
+        '''
+     WELCOME TO REDWOOD MANSION    
+    ============================
+    
+You are a lonely man searching for a cursed artifact, wishing that it could better your life. 
+You came across an abandoned mansion and decided to break in.
+Soon you hear screaming coming from the attic. You decide to investigate further. 
+You've heard about this place earlier and the rumors don't sound too good. 
+People say that the mansion is haunted by a monster and the monsters only job is killing you.
+    
+ Your mission:
+1. Explore the mansion
+2. Avoid monsters and traps
+3. Collect keys and other items
+4. Escape alive with the cursed artifact
+    
+Good luck...
+        '''
+    )
